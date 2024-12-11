4b71a9f8628520421bd8782df9edd5c6e5db92f1
0RlON <94333248+0RlON@users.noreply.github.com>
2024-12-11 21:48:53 +0000

    made changes to js

--- script.js
+++ script.js
@@ -5,27 +5,27 @@ function generateRandom() {
     resultParagraph.innerHTML = random;
 }
 
-sessionStorage.setItem("baseNumber", random)
+sessionStorage.setItem("baseRandom", random)
 var newRandom = Math.floor(Math.random() * 100 + 1);
 
-function higher() {
-    if (newRandom > sessionStorage.getItem("baseNumber")) {
+function higher(newRandom) {
+    if (newRandom > sessionStorage.getItem("baseRandom")) {
         window.alert("You Win!")
     } else {
         window.alert("You Lose!")
     }
 }
 
-function lower() {
-    if (newRandom < sessionStorage.getItem("baseNumber")) {
+function lower(newRandom) {
+    if (newRandom < sessionStorage.getItem("baseRandom")) {
         window.alert("You Win!")
     } else {
         window.alert("You Lose!")
     }
 }
 
-function sameNumber() {
-    if (newRandom == sessionStorage.getItem("baseNumber")) {
+function sameNumber(newRandom) {
+    if (newRandom == sessionStorage.getItem("baseRandom")) {
         window.alert("It was the same number, nothing happens!")
     }
 }
