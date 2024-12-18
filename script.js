63dfbf059cbbc0c457278eb9830d939c9a24fad8
0RlON <94333248+0RlON@users.noreply.github.com>
2024-12-18 00:26:50 +0000

    made changes to js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 function generateRandom() {
     var resultParagraph = document.getElementById("result");
     var random = Math.floor(Math.random() * 100 + 1);
-    console.log(random)
+    console.log(random);
     resultParagraph.innerHTML = random;
 }
 
-var oldRandom = random;
-var newRandom = Math.floor(Math.random() * 100 + 1);
-
 function higher() {
+    var oldRandom = random;
+    var newRandom = Math.floor(Math.random() * 100 + 1);
+    resultParagraph.innerHTML = newRandom;
     if (newRandom > oldRandom) {
-        window.alert("You Win!")
+        window.alert("You Win!");
     } else {
-        window.alert("You Lose!")
+        window.alert("You Lose!");
     }
 }
 
 function lower() {
     if (newRandom < oldRandom) {
-        window.alert("You Win!")
+        window.alert("You Win!");
     } else {
-        window.alert("You Lose!")
+        window.alert("You Lose!");
     }
+    console.log(newRandom);
+    resultParagraph.innerHTML = newRandom;
 }
 
 function sameNumber() {
+    resultParagraph.innerHTML = newRandom;
     if (newRandom == oldRandom) {
         window.alert("It was the same number, nothing happens!")
     }
